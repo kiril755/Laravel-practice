7c0f4c6628f502ce1b2b5deed90e8ea2b7b8683a
kiril755 <kiril777kiril@gmail.com>
2023-02-05 17:52:31 +0200

    practice w/ view layouts

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <nav>
+        <div>
+            <ul>
+                <li>
+                    <a href="{{ route('main.index') }}">Main</a>
+                </li>
+                <li>
+                    <a href="{{ route('post.index') }}">Posts</a>
+                </li>
+                <li>
+                    <a href="{{ route('about.index') }}">About</a>
+                </li>
+                <li>
+                    <a href="{{ route('contact.index') }}">Contacts</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    @yield('content')
+</body>
+</html>
