c71a715c2e8eea8420584210e3dd562606773378
kiril755 <kiril777kiril@gmail.com>
2023-02-06 00:06:01 +0200

    add bootstrap

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,27 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width= , initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    @vite(['resources/sass/app.scss', 'resources/js/app.js']) 
     <title>Document</title>
 </head>
 <body>
-    <nav>
-        <div>
-            <ul>
-                <li>
-                    <a href="{{ route('main.index') }}">Main</a>
-                </li>
-                <li>
-                    <a href="{{ route('post.index') }}">Posts</a>
-                </li>
-                <li>
-                    <a href="{{ route('about.index') }}">About</a>
-                </li>
-                <li>
-                    <a href="{{ route('contact.index') }}">Contacts</a>
-                </li>
-            </ul>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('main.index') }}">Main</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('post.index') }}">Posts</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('about.index') }}">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contact.index') }}">Contacts</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         </div>
-    </nav>
+    </div>
+    
     @yield('content')
 </body>
 </html>
